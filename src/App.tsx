26b217b8e50986f4476d9e06caad2d69fd4823e2
Diego <diannewdev@gmail.com>
2024-08-02 14:36:31 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+          <h1>Esta será minha segunda página.</h1>
+          <p>Esta é a inspiraçã para ela:</p>
+          <img src="src/assets/modelo.png" alt="" />
+    </div>
+  );
+};
+
+export default App;
